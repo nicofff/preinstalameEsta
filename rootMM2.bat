2792b016b35f10e790b2ed431515d86e827bb525
caesarivs <caesarivs@gmail.com>
2012-07-05 03:27:36 -0500

    Update master

--- rootMM2.bat
+++ rootMM2.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
+cls
 color 0c
 :restart
 cd "%~dp0"
-
 echo "__________                .__                 __         .__                        ___________         __          "
 echo "\______   \_______   ____ |__| ____   _______/  |______  |  | _____    _____   ____ \_   _____/ _______/  |______   "
 echo " |     ___/\_  __ \_/ __ \|  |/    \ /  ___/\   __\__  \ |  | \__  \  /     \_/ __ \ |    __)_ /  ___/\   __\__  \  "
@@ -15,59 +15,39 @@ echo "     \_ |__ ___.__.   ____ |__| ____  _____/ ____\/ ____\/ ____\          
 echo "      | __ <   |  |  /    \|  |/ ___\/  _ \   __\\   __\\   __\                                                     "
 echo "      | \_\ \___  | |   |  \  \  \__(  <_> )  |   |  |   |  |                                                       "
 echo "      |___  / ____| |___|  /__|\___  >____/|__|   |__|   |__|                                                       "
-echo "          \/\/           \/        \/                           "
-
-echo "Windows Instaler by Leito0o"
-
+echo "          \/\/           \/        \/                                                                               "
+echo.
+echo Windows Instaler by Leito0o
+echo.
 pause
 cls
-
-
 color 0b
 :restart
 cd "%~dp0"
-
-
-echo Waiting for Phone
+echo Waiting for Phone...
+echo.
 adb wait-for-device
-
-ADB devices
+adb devices
 adb push rooted.prop /data/preinstall_md5/
- adb push root.sh /data/preinstall_md5/
- adb shell "ln -s /sys/kernel/uevent_helper /data/preinstall_md5/first.md5"
- adb shell "ln -s /sys/class/mem/null/uevent /data/preinstall_md5/second.md5"
- adb shell "chmod 777 /data/preinstall_md5/root.sh"
-
-
-
-
+adb push root.sh /data/preinstall_md5/
+adb shell "ln -s /sys/kernel/uevent_helper /data/preinstall_md5/first.md5"
+adb shell "ln -s /sys/class/mem/null/uevent /data/preinstall_md5/second.md5"
+adb shell "chmod 777 /data/preinstall_md5/root.sh"
+echo.
 echo Sin cerrar la ventana del cmd, desconectar el celular , apagarlo 
-
 echo y reinicialo apretando el boton de power, el de la camara, y el de bajar el volumen ;
-
 echo Una vez que entres en modo bootloader planchar el fixed.sbf 
-
 echo (esta dentro de "firmware.7z") mediante rsdlite
-
 echo una vez que arranque, Activar la depuracion usb y conectar el celular, 
-
 echo luego presionar enter para continuar con el proceso 
-pause
-
-
-
-
-
-
-
-echo Waiting for Phone
+pause > nul
+echo.
+echo Waiting for Phone...
+echo.
 adb wait-for-device
-
-
 adb shell "echo 'entrando al adb, xq sino no funciona'"
 adb reboot
 adb wait-for-device
-
 adb remount
 adb push su /system/bin/su
 adb shell "chmod 6755 /system/bin/su"
@@ -78,12 +58,10 @@ adb shell "/system/xbin/busybox --install /system/xbin"
 adb push Superuser.apk /system/app/Superuser.apk
 adb shell "rm /data/preinstall_md5/*"
 adb shell "echo '/sbin/hotplug > /sys/kernel/uevent_helper'"
-
 adb reboot
 adb wait-for-device
 adb kill-server
-
-
-echo    FIN
-
+echo.
+echo FIN!
+echo.
 pause
